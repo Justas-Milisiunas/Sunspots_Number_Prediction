413a6570391390b98ccd4b035371a36514507c1b
JUSTAS-MILISIUNAS <justas.milisiunas@gmail.com>
2020-05-03 23:33:50 +0300

    Finished LD2

--- Graphs.py
+++ Graphs.py
@@ -7,11 +7,11 @@ from matplotlib.ticker import PercentFormatter
 
 def show_model_verification_plot(_data, title):
     _data = np.array(_data)
-    plt.plot(np.arange(len(_data)), _data[:, 0], color='r', label='Spėjimas')
-    plt.plot(np.arange(len(_data)), _data[:, 1], color='b', label='Tikėtasis')
+    plt.plot(np.arange(1700, 1700 + len(_data)), _data[:, 0], color='r', label='Spėjimas')
+    plt.plot(np.arange(1700, 1700 + len(_data)), _data[:, 1], color='b', label='Tikėtasis')
 
     plt.title(title)
-    plt.xlabel('Spėjimo nr.')
+    plt.xlabel('Metai')
     plt.ylabel('Saulės dėmių skaičiųs')
     plt.legend()
     plt.show()
@@ -48,7 +48,7 @@ def show_input_output_plot(_sunspots):
 
 
 def show_errors_histogram(_errors):
-    plt.hist(_errors, weights=np.ones(len(_errors)), bins=10)
+    plt.hist(_errors, weights=np.ones(len(_errors)), bins=10, ec='black')
 
     plt.title('Prognozės klaidų histograma')
     plt.xlabel('Paklaida')
@@ -57,10 +57,10 @@ def show_errors_histogram(_errors):
 
 
 def show_errors_plot(_errors):
-    plt.plot(_errors)
+    plt.plot(np.arange(1700, 1700 + len(_errors)), _errors)
 
     plt.title('Prognozės klaidų diagrama')
-    plt.xlabel('Prognozė')
+    plt.xlabel('Metai')
     plt.ylabel('Paklaida')
     plt.show()
 
@@ -91,86 +91,3 @@ def read_sunspots(file_name):
     with open(file_name, 'r') as f:
         all_sunspots = [row.replace('\n', '').split('\t') for row in f.readlines()]
         return [[int(sunspot[0]), int(sunspot[1])] for sunspot in all_sunspots]
-
-
-# sunspots = read_sunspots('sunspot.txt')
-# # show_input_output_plot(sunspots)
-#
-# data_min = min(np.array(sunspots)[:, 1])
-# data_max = max(np.array(sunspots)[:, 1])
-#
-# dataset = prepare_data(sunspots, 2)
-#
-# training_set = dataset[:200]
-# validation_set = dataset[200:]
-#
-# n_inputs = len(training_set[0]) - 1
-# n_outputs = 1
-#
-# network = NeuralNetwork(n_inputs, 1, n_outputs, data_min, data_max)
-# network.train(training_set, 0.01, 10000)
-#
-# MSE, MAD, errors, predictions = network.validate(validation_set)
-#
-# # show_errors_histogram(errors)
-# # show_errors_plot(errors)
-# show_model_verification_plot(predictions, title='Modelio verifikacija 1900-2014 metais')
-# print(f"MSE: {MSE} MAD: {MAD}")
-
-# np.random.seed(1)
-# network = initialize_network(n_inputs, 1, n_outputs)
-# print(network)
-# print("\n\n\n")
-# train_network(network, dataset, 0.01, 10000, n_outputs)
-# print(network)
-# errors = 0
-# max_error = 0
-# for row in x[200:]:
-#     prediction = predict(network, row)
-#     prediction = unnormalize_number(prediction, min_number, max_number)
-#     expected_output = unnormalize_number(row[-1], min_number, max_number)
-#     # print('Expected=%d, Got=%d' % (row[-1], prediction))
-#     error = np.abs(expected_output - prediction)
-#     if error >= max_error:
-#         max_error = error
-#     errors += (expected_output - prediction)**2
-#     print(f"Expected={expected_output} Got={prediction}")
-#
-# print(f"Errors sum: {errors/len(x[200:])} Errors average: {errors / len(x[200:])} Max error: {max_error}")
-
-# plt.plot([sunspot[0] for sunspot in sunspots], [sunspot[1] for sunspot in sunspots])
-# plt.title("Saulės dėmių aktyvumo 1700-2014 m. grafikas")
-# plt.xlabel('Metai')
-# plt.ylabel('Saulės dėmių sk.')
-# plt.show()
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# # for i in range(columns):
-# #     ax.scatter(x[:, i], y)
-#
-# # ax.scatter(x[:, 0], x[:, 1], x[:, 2], x[:, 3], y)
-# ax.scatter(P[:, 0], P[:, 1], T)
-# plt.show()
-#
-# import neurolab as nl
-#
-# # Logical &
-# input = [[0, 0], [0, 1], [1, 0], [1, 1]]
-# target = [[0], [0], [0], [1]]
-#
-# # Create net with 2 inputs and 1 neuron
-# net = nl.net.newp([[0, 1], [0, 1]], 1)
-#
-# # train with delta rule
-# # see net.trainf
-# error = net.trainf(input, target, epochs=100, show=10, lr=0.1)
-#
-# # Plot results
-# import pylab as pl
-#
-# pl.plot(error)
-# pl.xlabel('Epoch number')
-# pl.ylabel('Train error')
-# pl.grid()
-# pl.show()
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Sunspots_Number_Prediction
+
+**Task**
+
+![img1](https://i.imgur.com/UeWmmau.png)
+
+**Report**
+
+![img2](https://i.imgur.com/7pWP70i.png)
+
+![img2](https://i.imgur.com/HjPCvJ7.png)
+
+![img2](https://i.imgur.com/wCAW16I.png)
+
+![img2](https://i.imgur.com/APlaKCy.png)
+
+![img2](https://i.imgur.com/xDIqzIm.png)
+
+![img2](https://i.imgur.com/ZThaUOx.png)
+
+![img2](https://i.imgur.com/FaNKw17.png)
+
+![img2](https://i.imgur.com/TNGKGaq.png)
